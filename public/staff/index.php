56384915d01237481fd8ed91e79f6b8f7b753438
Ahmet Kutay Karacair <akkaracair@gmail.com>
2022-02-08 22:59:57 +0300

    updated and added some files in subjects and pages, added control function for special char or file directory for link

--- public/staff/index.php
+++ public/staff/index.php
@@ -2,5 +2,13 @@
 
 <?php $page_title = "Staff - Menu" ?>
 <?php include(SHARED_PATH.'/staff_header.php'); ?>
-<div id="content"></div>
+<div id="content">
+    <div id="main-menu">
+        <ul>
+            <h2>Main Menu</h2>
+            <li><a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a> </li>
+            <li><a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a> </li>
+        </ul>
+    </div>
+</div>
 <?php include(SHARED_PATH.'/staff_footer.php'); ?>
