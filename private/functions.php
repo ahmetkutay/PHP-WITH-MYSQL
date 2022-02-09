cb39d7f6d2fe5859e0aade69e5273c416be26753
Ahmet Kutay Karacair <39398963+ahmetkutay@users.noreply.github.com>
2022-02-09 21:30:44 +0300

    added post and get methods, create and edit files for subjects

--- private/functions.php
+++ private/functions.php
@@ -40,6 +40,13 @@ function redirect()
     header("Location: ".url_for('/staff/subjects/index.php'));
     exit();
 }
+function is_post_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'POST';
+}
+
+function is_get_request() {
+    return $_SERVER['REQUEST_METHOD'] == 'GET';
+}
 
 
 
